f8e9852bd1e1a7569771e0be89794291aac0f717
Carlos Vidal <nakioparkour@gmail.com>
2016-07-03 16:51:57 +0100

    AppKit extension of Item

---
+++ EasyPeasy/Item+AppKit.swift
@@ -0,0 +1,44 @@
+// The MIT License (MIT) - Copyright (c) 2016 Carlos Vidal
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#if !(os(iOS) || os(tvOS))
+    
+import AppKit
+
+/// Alias of NSView
+public typealias View = NSView
+
+/**
+     Item protocol extension implementing some convenience properties
+ */
+internal extension Item {
+    
+    /// Owning `UIView` for the current `Item`. The concept varies
+    /// depending on the class conforming the protocol
+    internal var owningView: View? {
+        get {
+            // Owning view for `UIView` is the `superview`
+            if let view = self as? View {
+                return view.superview
+            }
+            
+            return nil
+        }
+    }
+    
+}
+
+/**
+     Extension making `UIView` conform the `Item` protocol and
+     therefore inherit the extended methods and properties
+ */
+extension NSView: Item { }
+    
+#endif
