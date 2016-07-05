dfd545f472a346969dfc34d9311583c58d3886eb
Carlos Vidal <nakioparkour@gmail.com>
2016-07-05 22:27:13 +0100

    updated assets

---
+++ Example/Demo macOS/Demo macOS/TweetModel.swift
@@ -0,0 +1,65 @@
+// The MIT License (MIT) - Copyright (c) 2016 Carlos Vidal
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import AppKit
+
+struct TweetModel {
+    
+    let name: String
+    let username: String
+    let displayableDate: String
+    let tweet: String
+    let thumbnail: NSImage?
+    
+    init(name: String, username: String, displayableDate: String, tweet: String, thumbnail: NSImage?) {
+        self.name = name
+        self.username = username
+        self.displayableDate = displayableDate
+        self.tweet = tweet
+        self.thumbnail = thumbnail
+    }
+    
+}
+
+extension TweetModel {
+    
+    static func stubData() -> [TweetModel] {
+        let tweetFelix = TweetModel(
+            name: "Felix Krause",
+            username: "@KrauseFX",
+            displayableDate: "30m",
+            tweet: "With Fastlane nobody has to deal with xcodebuild anymore. Say goodbye to NSLayoutConstraints thanks to EasyPeasy 🚀",
+            thumbnail: nil
+        )
+        let tweetEloy = TweetModel(
+            name: "Eloy Durán",
+            username: "@alloy",
+            displayableDate: "1h",
+            tweet: "EasyPeasy, best thing since CocoaPods socks were announced!",
+            thumbnail: nil
+        )
+        let tweetJavi = TweetModel(
+            name: "Javi.swift",
+            username: "@Javi",
+            displayableDate: "2h",
+            tweet: "EasyPeasy? another autolayout library? Okay, I can give it a try!",
+            thumbnail: nil
+        )
+        let tweetNacho = TweetModel(
+            name: "NeoGazpatchOS",
+            username: "@NeoNacho",
+            displayableDate: "4h",
+            tweet: "Just discovered EasyPeasy... silly name, great framework #yatusabes",
+            thumbnail: nil
+        )
+        return [tweetFelix, tweetEloy, tweetJavi, tweetNacho]
+    }
+    
+}
