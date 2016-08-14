e069af5335b455a350df8576890eff585c93fa6c
Carlos Vidal <nakioparkour@gmail.com>
2016-08-14 12:47:29 +0100

    moved constraint deactivation to Item level

--- EasyPeasy/Node.swift
+++ EasyPeasy/Node.swift
@@ -62,14 +62,17 @@ internal class Node {
         `Node` and its associated `NSLayoutConstraint` returned in 
         order to be activated by the `Item` owning the `Node`.
         - parameter attribute: `Attribute` to be added to the `Node`
-        - returns: `NSLayoutConstraints` to be activated by the 
-        `Item` owning the current `Node`
+        - returns an `ActivationGroup` gathering `NSLayoutConstraints` 
+        to be activated/deactivated by the `Item` owning the current 
+        `Node`
      */
-    func add(attribute attribute: Attribute) -> [NSLayoutConstraint] {
+    func add(attribute attribute: Attribute) -> ActivationGroup? {
         guard attribute.shouldInstall() else {
             self.inactiveAttributes.append(attribute)
-            return []
+            return nil
         }
+        
+        var deactivateConstraints: [NSLayoutConstraint] = []
         
         // Checks whether the `Attribute` is conflicting with any of
         // the existing `Subnodes`. If so deactivates the conflicting
@@ -77,21 +80,29 @@ internal class Node {
         let nodeAttribute = attribute.createAttribute.subnode
         switch nodeAttribute {
         case .Left:
-            if self.left === attribute { return [] }
-            self.deactivate(attributes: [self.left, self.center].flatMap { $0 })
+            if self.left === attribute { return nil }
+            deactivateConstraints.appendContentsOf(
+                self.deactivate(attributes: [self.left, self.center].flatMap { $0 })
+            )
             self.left = attribute
         case .Right:
-            if self.right === attribute { return [] }
-            self.deactivate(attributes: [self.right, self.center].flatMap { $0 })
+            if self.right === attribute { return nil }
+            deactivateConstraints.appendContentsOf(
+                self.deactivate(attributes: [self.right, self.center].flatMap { $0 })
+            )
             self.right = attribute
         case .Center:
-            if self.center === attribute { return [] }
-            self.deactivate(attributes: [self.center, self.left, self.right].flatMap { $0 })
+            if self.center === attribute { return nil }
+            deactivateConstraints.appendContentsOf(
+                self.deactivate(attributes: [self.center, self.left, self.right].flatMap { $0 })
+            )
             self.center = attribute
         case .Dimension:
-            if self.dimension === attribute { return [] }
+            if self.dimension === attribute { return nil }
             if let previousDimension = self.dimension {
-                self.deactivate(attributes: [previousDimension])
+                deactivateConstraints.appendContentsOf(
+                    self.deactivate(attributes: [previousDimension])
+                )
             }
             self.dimension = attribute
         }
@@ -99,20 +110,21 @@ internal class Node {
         // Returns the associated `NSLayoutConstraint` to be activated
         // by the `Item` owning the `Node`
         if let layoutConstraint = attribute.layoutConstraint {
-            return [layoutConstraint]
+            return ([layoutConstraint], deactivateConstraints)
         }
         
-        return []
+        return ([], deactivateConstraints)
     }
     
     /**
-        Deactivates the `NSLayoutConstraints` for the `Attributes`
-        given. Also nullifies the `Subnodes` for those `Attributes`
+        Returns the `NSLayoutConstraints` to deactivate for the `Attributes`
+        given. Also nullifies the `Subnodes` holding those `Attributes`
         - parameter attributes: `Attributes` to be deactivated
+        - returns an array of `NSLayoutConstraints` to be deactivated
      */
-    func deactivate(attributes attributes: [Attribute]) {
+    func deactivate(attributes attributes: [Attribute]) -> [NSLayoutConstraint] {
         guard attributes.count > 0 else {
-            return
+            return []
         }
         
         var layoutConstraints: [NSLayoutConstraint] = []
@@ -131,24 +143,27 @@ internal class Node {
             }
         }
         
-        // Deactivate `NSLayoutContraints`
-        NSLayoutConstraint.deactivateConstraints(layoutConstraints)
+        // Return `NSLayoutContraints` to deactivate
+        return layoutConstraints
     }
     
     /**
-        Re-evaluates every `Condition` closure within the active and
-        inactive `Attributes`, in case an active `Attribute` has 
-        become inactive deactivates it and the `NSLayoutConstraints` of
-        those that have changed to active are passed to the `Item` owner
-        of the `Node` to active them along with other `Nodes` active
-        `NSLayoutConstraints`
-        - returns the `NSLayoutConstraints` to be activated
+        Re-evaluates every `Condition` closure within the active and inactive
+        `Attributes`, in case an active `Attribute` has become inactive 
+        returns its associated `NSLayoutConstraints` along with those that
+        have changed to active in order to be activated or deactivated by the
+        `Item` owning the `Node`
+        - returns an `ActivationGroup` gathering the `NSLayoutConstraints` 
+        to be activated and deactivated
      */
-    func reload() -> [NSLayoutConstraint] {
+    func reload() -> ActivationGroup {
+        var activateConstraints: [NSLayoutConstraint] = []
+        var deactivateConstraints: [NSLayoutConstraint] = []
         
-        // Deactivate `Attributes` which condition changed to false
+        // Get the `Attributes` its condition changed to false in order to
+        // deactivate the associated `NSLayoutConstraint`
         let deactivatedAttributes = self.activeAttributes.filter { $0.shouldInstall() == false }
-        self.deactivate(attributes: deactivatedAttributes)
+        deactivateConstraints.appendContentsOf(self.deactivate(attributes: deactivatedAttributes))
         
         // Gather all the existing `Attributes` that need to be added
         // again to the `Node`
@@ -161,18 +176,26 @@ internal class Node {
     
         // Re-add `Attributes` to the `Node` in order to solve conflicts
         // and re-evaluate `Conditions`
-        let layoutAttributes = activeAttributes.flatMap { self.add(attribute: $0) }
+        activeAttributes.forEach { attribute in
+            if let activationGroup = self.add(attribute: attribute) {
+                activateConstraints.appendContentsOf(activationGroup.0)
+                deactivateConstraints.appendContentsOf(activationGroup.1)
+            }
+        }
         
-        return layoutAttributes
+        return (activateConstraints, deactivateConstraints)
     }
     
     /**
-        Deactives all the active `Attributes` within the node and
-        clears all the persisted ones
+        Returns all the active `NSLayoutConstraints` within the node and
+        clears all the persisted `Attributes`
+        - returns an array of `NSLayoutConstraints` to deactivate
      */
-    func clear() {
-        self.deactivate(attributes: self.activeAttributes)
+    func clear() -> [NSLayoutConstraint] {
+        let deactivateConstraints = self.deactivate(attributes: self.activeAttributes)
         self.inactiveAttributes = []
+        
+        return deactivateConstraints
     }
     
 }
