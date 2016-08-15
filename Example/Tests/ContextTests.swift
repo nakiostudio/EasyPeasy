8d40c6fe6693885bee4a2fb34e0a3582faf72d5d
Carlos Vidal <nakioparkour@gmail.com>
2016-08-15 20:04:23 +0100

    context tests

---
+++ Example/Tests/ContextTests.swift
@@ -0,0 +1,50 @@
+// The MIT License (MIT) - Copyright (c) 2016 Carlos Vidal
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import XCTest
+@testable import EasyPeasy
+
+class ContextTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testThatContextIsInitializedWithTheExpectedValues() {
+        // given
+        let view = UIView()
+        let isPad = UI_USER_INTERFACE_IDIOM() == .Pad
+        let isPhone = UI_USER_INTERFACE_IDIOM() == .Phone
+        let isCompact = view.traitCollection.verticalSizeClass == .Compact && view.traitCollection.horizontalSizeClass == .Compact
+        let isHorizontalCompact = view.traitCollection.horizontalSizeClass == .Compact
+        let isVerticalCompact = view.traitCollection.verticalSizeClass == .Compact
+        let isRegular = view.traitCollection.verticalSizeClass == .Regular && view.traitCollection.horizontalSizeClass == .Regular
+        let isHorizontalRegular = view.traitCollection.horizontalSizeClass == .Regular
+        let isVerticalRegular = view.traitCollection.verticalSizeClass == .Regular
+        
+        // when
+        let context = Context(with: view.traitCollection)
+        
+        // then
+        XCTAssertTrue(context.isPad == isPad)
+        XCTAssertTrue(context.isPhone == isPhone)
+        XCTAssertTrue(context.isHorizontalVerticalCompact == isCompact)
+        XCTAssertTrue(context.isHorizontalCompact == isHorizontalCompact)
+        XCTAssertTrue(context.isVerticalCompact == isVerticalCompact)
+        XCTAssertTrue(context.isHorizontalVerticalRegular == isRegular)
+        XCTAssertTrue(context.isHorizontalRegular == isHorizontalRegular)
+        XCTAssertTrue(context.isVerticalRegular == isVerticalRegular)
+    }
+    
+}
